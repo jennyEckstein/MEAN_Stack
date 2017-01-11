39f2233c620c6a3a3e35c2173e90d22d887a3ad4
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2017-01-10 22:55:06 -0500

    feat: property and event binding

--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+
+import { Message } from "./message.model";
 
 @Component({
 	selector: 'app-message',
@@ -20,8 +22,10 @@ import { Component } from "@angular/core";
 })
 
 export class MessageComponent{
-	message = {
-    	content: 'A message',
-    	author: 'Jenny'
-    };
+	@Input() message: Message;
+	@Output() editClicked = new EventEmitter<string>();
+
+	onEdit(){
+		this.editClicked.emit('A new value');
+	}
 }
