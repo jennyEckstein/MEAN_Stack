33e644faba5f9004bc36356a817eee15b57b5c5c
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2017-01-15 21:09:30 -0500

    feat: added routing

---
+++ assets/app/auth/authentication.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: 'app-authentication',
+	template: `<h1>Auth Component</h1>`
+
+})
+
+export class AuthenticationComponent {
+
+}
