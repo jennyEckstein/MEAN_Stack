b25841b8ed13eceb0a9b94061454adb5c31efe89
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2017-01-15 21:50:21 -0500

    feat: adding child routes

--- assets/app/auth/authentication.component.ts
+++ assets/app/auth/authentication.component.ts
@@ -2,8 +2,21 @@ import { Component } from "@angular/core";
 
 @Component({
 	selector: 'app-authentication',
-	template: `<h1>Auth Component</h1>`
+	template: `
+		<header class="row spacing">
+			<nav class="col-md-8 col-md-offset-2">
+				<ul class="nav nav-tabs">
+					<li routerLinkActive="active"><a [routerLink]="['signup']">SignUp</a></li>
+					<li routerLinkActive="active"><a [routerLink]="['signin']">SignIn</a></li>
+					<li routerLinkActive="active"><a [routerLink]="['logout']">Logout</a></li>
+				</ul>
+			</nav>
+		</header>
 
+		<div class="row spacing">
+			<router-outlet></router-outlet>
+		</div>
+	`
 })
 
 export class AuthenticationComponent {
