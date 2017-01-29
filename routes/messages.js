6c4bf5d0aa4aa4cfd05e15c5c7f12682feca5e55
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2017-01-29 16:00:36 -0500

    fix: stop users deleting messages created by other users

--- routes/messages.js
+++ routes/messages.js
@@ -66,6 +66,7 @@ router.post('/', function(req, res, next){
 
 
 router.patch('/:id', function(req, res, next){
+	var decoded = jwt.decode(req.query.token);
 	console.log("Patching Backend");
 	Message.findById(req.params.id, function(err, message){
 		if (err){
@@ -79,6 +80,12 @@ router.patch('/:id', function(req, res, next){
 				title: 'No Message Found',
 				error: {message: 'Message not found'}
 			});			
+		}
+		if(message.user != decoded.user._id){			
+			return res.status(401).json({
+				title: 'Not Authenticated',
+				error: {message: 'Users do not match '}
+			});		
 		}
 		message.content = req.body.content;
 		message.save(function(err, result){
@@ -97,6 +104,7 @@ router.patch('/:id', function(req, res, next){
 });
 
 router.delete('/:id', function(req, res, next){
+	var decoded = jwt.decode(req.query.token);
 	Message.findById(req.params.id, function(err, message){
 		if (err){
 			return res.status(500).json({
@@ -117,6 +125,12 @@ router.delete('/:id', function(req, res, next){
 					error: err
 				});
 			}
+			if(message.user != decoded.user._id){			
+			return res.status(401).json({
+				title: 'Not Authenticated',
+				error: {message: 'Users do not match '}
+			});		
+		}
 			res.status(200).json({
 				message: 'Deleted Message',
 				obj: result
